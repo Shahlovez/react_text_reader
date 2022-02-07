4615895bb1d8c25ef5fe88f4b87b0ef89bc12796
Shahlo Ovezniyazova <ovezniyazova@gmail.com>
2022-02-07 12:00:58 -0500

    image description

--- src/App.js
+++ src/App.js
@@ -56,16 +56,31 @@ const dataActions = [
    {id: 11,
     message:"Listen to music",
     url:"https://images.unsplash.com/photo-1518609878373-06d740f60d8b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8bXVzaWN8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"},
-   {url:"https://media.istockphoto.com/photos/boxes-with-clothes-for-donation-in-home-interior-picture-id1321296846?b=1&k=20&m=1321296846&s=170667a&w=0&h=CZq1VC0HkUIF0-bikwYoWlzwpQVzkZ_BlrVS-3_OlV8="},
+   
+    {id: 12,
+    message:"Share",
+    url:"https://media.istockphoto.com/photos/boxes-with-clothes-for-donation-in-home-interior-picture-id1321296846?b=1&k=20&m=1321296846&s=170667a&w=0&h=CZq1VC0HkUIF0-bikwYoWlzwpQVzkZ_BlrVS-3_OlV8="},
 ]
 
 const dataRandom = [
-{url:"https://images.unsplash.com/photo-1596461404969-9ae70f2830c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8dG95fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"},
-{url: "https://images.unsplash.com/photo-1570913149827-d2ac84ab3f9a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YXBwbGV8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"},
-{url:"https://images.unsplash.com/photo-1541781774459-bb2af2f05b55?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8c2xlZXB8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"},
-{url:"https://images.unsplash.com/photo-1491309055486-24ae511c15c7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8cmVhZGluZ3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60"},
-{url:"https://images.unsplash.com/photo-1568828668638-b1b4014d91a2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8bnVtYmVyc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60"},
-{url:"https://images.unsplash.com/photo-1464349153735-7db50ed83c84?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fGJpcnRoZGF5fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"},
+{id: 13,
+  message:"Can't find my toy",
+  url:"https://images.unsplash.com/photo-1596461404969-9ae70f2830c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8dG95fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"},
+{id: 14,
+  message:"An apple",
+  url: "https://images.unsplash.com/photo-1570913149827-d2ac84ab3f9a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YXBwbGV8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"},
+{id: 15,
+  message:"Sleep",
+  url:"https://images.unsplash.com/photo-1541781774459-bb2af2f05b55?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8c2xlZXB8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"},
+{id: 16,
+  message:"Read a book",
+  url:"https://images.unsplash.com/photo-1491309055486-24ae511c15c7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8cmVhZGluZ3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60"},
+{ id: 17,
+    message:"Number blocks",
+    url:"https://images.unsplash.com/photo-1568828668638-b1b4014d91a2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8bnVtYmVyc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60"},
+{id: 18,
+  message:"Celebrating Birthday",
+  url:"https://images.unsplash.com/photo-1464349153735-7db50ed83c84?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fGJpcnRoZGF5fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"},
 ]
 
 
